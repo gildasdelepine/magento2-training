87be9a3b8b251c36ca227b0c8808c9029fcd1602
Gildas Delepine <gildas.delepine@smile.fr>
2018-09-11 10:06:37 +0200

    05 - Concepts Injection de dépendances Creation Controller Product

---
+++ app/code/Training/Helloworld/Controller/Product/Index.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: training
+ * Date: 9/10/18
+ * Time: 5:15 PM
+ */
+namespace Training\Helloworld\Controller\Product;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Catalog\Model\ProductFactory as ProductFactory;
+use Magento\Catalog\Model\Product as Product;
+use Magento\Framework\Exception\NotFoundException;
+
+class Index extends Action
+{
+    /**
+     * @var ProductFactory
+     */
+    protected $productFactory;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param ProductFactory $productFactory
+     */
+    public function __construct(Context $context, ProductFactory $productFactory)
+    {
+        parent::__construct($context);
+        $this->productFactory = $productFactory;
+    }
+
+
+    /**
+     * Get the requested product
+     *
+     * @return Product|null
+     * @throws NotFoundException
+     */
+    protected function getProduct() {
+        // requested id - need to be (int) => cast
+        $pid = (int) $this->getRequest()->getParam('id');
+        if(!$pid) {
+            throw new NotFoundException(__('product id not found'));
+        }
+
+        // get the product
+        $product = $this->productFactory->create();
+        $product->getResource()->load($product, $pid);
+
+        if(!$product->getId()) {
+            throw new NotFoundException(__('product not found'));
+        }
+
+        return $product;
+
+    }
+
+    /**
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+
+        $product = $this->getProduct();
+
+        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        $result->setContents('Product : '. $product->getName());
+
+        return $result;
+    }
+}
