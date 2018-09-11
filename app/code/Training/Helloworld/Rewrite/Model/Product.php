19fd73d6d6dece4a62205c9c5905050e6c0adccf
Gildas Delepine <gildas.delepine@smile.fr>
2018-09-11 14:11:34 +0200

    05 - Concepts Rewrite Model Catalog Product

---
+++ app/code/Training/Helloworld/Rewrite/Model/Product.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Rewrite Product class
+ */
+namespace Training\Helloworld\Rewrite\Model;
+
+use \Magento\Catalog\Model\Product as BaseProduct;
+
+class Product extends BaseProduct
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return parent::getName() . ' (Hello World !)';
+    }
+}
