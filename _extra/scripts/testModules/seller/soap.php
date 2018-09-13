23c25949a09feec1ed8b7ef8276ced0e3559be5f
Gildas Delepine <gildas.delepine@smile.fr>
2018-09-13 10:06:31 +0200

    06 - Models Seller - Part 1 Test soap.php, full.php OK (slide 223)

--- _extra/scripts/testModules/seller/soap.php
+++ _extra/scripts/testModules/seller/soap.php
@@ -5,8 +5,40 @@ require __DIR__.'/../_tools/init.php';
 $client = new \SmileCoreTest\SoapClient();
 $client->setDebug(true);
 $client->setMagentoParams($params);
-$client->addService('sellerSellerRepositoryV1');
+$client->addService('trainingSellerSellerRepositoryV1');
 
-$client->sellerSellerRepositoryV1GetById(['customerId' => 1]);
+$client->trainingSellerSellerRepositoryV1GetById(['objectId' => 1]);
+$client->trainingSellerSellerRepositoryV1GetByIdentifier(['objectIdentifier' => 'main']);
 
 
+$object = [
+    'object' => [
+        'identifier' => 'cots',
+        'name'       => 'Constantin Tsiolkovski',
+    ]
+];
+
+$search = [
+    'searchCriteria' => [
+        'filterGroups' => [
+            [
+                'filters' => [
+                    [
+                        'field'          => 'identifier',
+                        'conditionType'  => 'like',
+                        'value'          => '%cot%'
+                    ]
+                ]
+            ]
+        ]
+    ],
+];
+
+$client->trainingSellerSellerRepositoryV1Save($object);
+$client->trainingSellerSellerRepositoryV1GetByIdentifier(['objectIdentifier' => $object['object']['identifier']]);
+$client->trainingSellerSellerRepositoryV1DeleteByIdentifier(['objectIdentifier' => $object['object']['identifier']]);
+
+$sp = $client->trainingSellerSellerRepositoryV1Save($object);
+$client->trainingSellerSellerRepositoryV1GetById(['objectId' => $sp->sellerId]);
+$client->trainingSellerSellerRepositoryV1GetList($search);
+$client->trainingSellerSellerRepositoryV1DeleteById(['objectId' => $sp->sellerId]);
